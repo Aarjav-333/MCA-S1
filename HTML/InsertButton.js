9c22debcbc52034bc4870c621cfc25050e828557
ajitali <ajitabi123@gmail.com>
2025-10-06 09:49:11 +0530

    College Thurannappol

---
+++ HTML/InsertButton.js
@@ -0,0 +1,14 @@
+// document.getElementById("btn-1").addEventListener("click", function() {
+//     var rcol = "#";
+//     for (let i = 0; i < 6; i++) {
+//         rcol += Math.floor(Math.random() * 10)
+        
+//     }
+//     document.body.style.background = rcol;
+
+
+// });
+
+
+const cars = ["bmw", "mercedes", "koenigsegg", "audi"];
+console.log(cars[0]);
