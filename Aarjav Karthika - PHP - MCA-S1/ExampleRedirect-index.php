12a5346025590eecba5f92f785ff9d651edeae78
ajitali <ajitabi123@gmail.com>
2025-10-27 10:33:59 +0530

    New Example

--- Aarjav Karthika - PHP - MCA-S1/ExampleRedirect-index.php
+++ Aarjav Karthika - PHP - MCA-S1/ExampleRedirect-index.php
@@ -5,7 +5,7 @@
 </head>
 <body>
     <h2>Enter Your Name</h2>
-    <form action="ExampleRedirect-welcome.php" method="GET">
+    <form action="ExampleRedirect-welcome.php" method="POST">
         <label for="name">Name:</label>
         <input type="text" name="name" id="name" required>
         <input type="submit" value="Submit">
--- Aarjav Karthika - PHP - MCA-S1/ExampleRedirect-welcome.php
+++ Aarjav Karthika - PHP - MCA-S1/ExampleRedirect-welcome.php
@@ -5,10 +5,12 @@
 </head>
 <body>
     <?php
-    if (isset($_GET['name'])) {
-        $name = htmlspecialchars($_GET['name']); 
+    // Check if the form data is received
+    if (isset($_POST['name'])) {
+        $name = htmlspecialchars($_POST['name']); // Prevent XSS
+        echo "<h2>Welcome, $name!</h2>";
     } else {
-        echo "<h2>No name provided.</h2>";
+        echo "<h2>No name provided. Please go back and enter your name.</h2>";
     }
     ?>
 </body>
