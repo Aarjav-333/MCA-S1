7766d3c21536b49fafdc263a13bad41c6b3754d3
ajitali <ajitabi123@gmail.com>
2025-10-27 10:22:57 +0530

    New Example

--- Aarjav Karthika - PHP - MCA-S1/ExampleRedirect-welcome.php
+++ Aarjav Karthika - PHP - MCA-S1/ExampleRedirect-welcome.php
@@ -5,10 +5,8 @@
 </head>
 <body>
     <?php
-    // Check if the form data is received
     if (isset($_GET['name'])) {
-        $name = htmlspecialchars($_GET['name']); // Prevent XSS
-        echo "<h2>Welcome, $name!</h2>";
+        $name = htmlspecialchars($_GET['name']); 
     } else {
         echo "<h2>No name provided. Please go back and enter your name.</h2>";
     }
