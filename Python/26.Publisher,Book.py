bf39acbeb1203d42a9dd11c5b7bf3b7c28118c23
ajitali <ajitabi123@gmail.com>
2025-10-29 09:56:25 +0530

    new day new commit

--- Python/26.Publisher,Book.py
+++ Python/26.Publisher,Book.py
@@ -1,4 +1,3 @@
-# Base class
 class Publisher:
     def __init__(self, name):
         self.name = name
@@ -8,24 +7,19 @@ class Publisher:
         print(f"Publisher Name: {self.name}")
 
 
-# Derived class
 class Book(Publisher):
     def __init__(self, name, title, author):
-        # Invoke base class constructor
         super().__init__(name)
         self.title = title
         self.author = author
         print("Book constructor called")
 
-    # Overriding the base class method
     def display(self):
-        # Optionally, call base class version
         super().display()
         print(f"Book Title: {self.title}")
         print(f"Author: {self.author}")
 
 
-# Example usage
 b = Book("Penguin Random House", "The Great Gatsby", "F. Scott Fitzgerald")
 print("\nDisplaying details:\n")
 b.display()
