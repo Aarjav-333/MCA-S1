0a3000b3606ab40df125f792b3c935de8b66551f
ajitali <ajitabi123@gmail.com>
2025-11-07 15:22:14 +0530

    PHP student registration - completed

--- PHP/StudentAdminLogin/db.php
+++ PHP/StudentAdminLogin/db.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$dbname = "student_db";
+$dbname = "karthika_db";
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 if ($conn->connect_error) {
